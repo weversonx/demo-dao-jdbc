67fdcbe542c3f79e86d8d2d5adff95d27ce07515
Weverson <wevercb11@gmail.com>
2022-06-29 13:48:28 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,5 +37,16 @@ SellerDao sellerDao = DaoFactory.createSellerDao();
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("update completed!");
+		
+		
+		
+		
 	}
+	
+
 }
